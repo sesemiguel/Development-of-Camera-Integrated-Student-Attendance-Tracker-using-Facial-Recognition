ab02b2dddb713f51a83deaeaf29fcce589bc4be1
sesem <sesem@Sese>
2018-04-09 23:31:23 +0800

    New Commit

---
+++ src/lbypc30_project/FaceModel.java
@@ -0,0 +1,19 @@
+package lbypc30_project;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+
+public class FaceModel {
+	public Point leftEye;
+	public Point rightEye;
+	public Mat cropedFace;
+
+	public FaceModel() {
+
+	}
+
+	public FaceModel(Point leftEye, Point rightEye) {
+		this.leftEye = leftEye;
+		this.rightEye = rightEye;
+
+	}
+}
